4d6a976dd188a40191cefd04fbb71c9aa1538b80
Travis Arnold <ftntravis@gmail.com>
2014-11-12 07:25:54 -0800

    updated expose modal plugin, deleted old file

--- app/scripts/plugins/jquery.expose.js
+++ app/scripts/plugins/jquery.expose.js
@@ -1,53 +1,77 @@
-;+function($) {
+$.fn.expose = function () {
 
-  'use strict';
-
-  $.fn.expose = function() {
-    
+    'use strict';
+  
     var $modal   = $(this),
-        $modalBG = $('<div class="modal_bg">'),
+        $doc     = $(document),
         $body    = $('body'),
-        $trigger = $('a[href=' + this.selector + ']');
-    
-    $modal.prepend($modalBG);
-    
-    $modal.on('expose:open', function() {
-      
-      $body.addClass('modal--open');
-      $modal.trigger('expose:opened');
+        $trigger = $('a[href=#' + $(this).attr('id') + ']'),
 
-      setTimeout(function() {
-        $body.css('overflow', 'hidden');
-      }, 500);
-    });
-    
-    $modal.on('expose:close', function() {
-      
-      $body.removeClass('modal--open');
-      $modal.trigger('expose:closed');
+        _scrollTop = 0;
+  
+    // add close button to modal
+    $modal.append('<i class="modal__close" />');
 
-      setTimeout(function() {
-        $body.css('overflow', '');
-      }, 500);
+    // open modal functionality
+    $modal.on('expose:open', function () {
+
+        // open the modal
+        $modal.addClass('modal--open');
+
+        // grab current scroll position
+        _scrollTop = $(window).scrollTop();
+
+        // set the body to fixed and position it where scrollbar was
+        $body.css('top', -_scrollTop + 'px').addClass('fixed');
+
+        // trigger "opened" callback
+        $modal.trigger('expose:opened');
     });
-    
-    $trigger.on('click', function(e) {
-      
-      e.preventDefault();
-      $modal.trigger('expose:open');
+
+    // close modal functionality
+    $modal.on('expose:close', function () {
+
+        // remove fixed position and set back to normal
+        $body.removeClass('fixed').css('top', '');
+
+        // position the page back to where it was
+        window.scroll(0, _scrollTop);
+
+        // close the modal
+        $modal.removeClass('modal--open');
+
+        // trigger "closed" callback
+        $modal.trigger('expose:closed');
     });
-    
-    $modal.add( $modal.find('.modal_close') ).on('click', function(e) {
-      
-      e.preventDefault();
-      
-      // if it isn't the background or close button, bail
-      if( e.target !== this )
-        return;
-      
-      $modal.trigger('expose:close');
+
+    // launch modal on click
+    $trigger.on('click', function (e) {
+
+        e.preventDefault();
+        $modal.trigger('expose:open');
     });
-    
-    return;
-  };
-}(jQuery);
+
+    // close modal with close button or by clicking bg
+    $modal.add( $modal.find('.modal__close') ).on('click', function (e) {
+
+        e.preventDefault();
+
+        // if it isn't the background or close button, bail
+        if(e.target !== this) {
+            return;
+        }
+
+        $modal.trigger('expose:close');
+    });
+
+    // close modal with ESC key
+    $doc.on('keydown keyup', function (e) {
+
+        if(e.which === 27) {
+            e.preventDefault();
+            $modal.trigger('expose:close');
+        }
+    });
+
+    return this;
+};
